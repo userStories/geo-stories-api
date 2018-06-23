cc9d8d2f8d9481fef83d5d5ae67c3dd984cf278b
Dan Gutt <dgutt10@gmail.com>
2018-06-23 11:59:23 -0500

    modified seed data

--- script/data.js
+++ script/data.js
@@ -2,7 +2,7 @@ const Posts = [
     {id: 1, latitude: 41.89536942, longitude: -87.63687061, mediaType: "image", title: "Beer Fest", text: "German beer festival", mediaLink: "https://cdn.muenchen-p.de/.imaging/stk/responsive/galleryPreview980/dms/wiesn/2017/exithamster/exithamster/zelte-hp/document/zelte-hp.jpg"},
     {id: 2, latitude: 41.89470126, longitude: -87.63738955, mediaType: "image", title: "Tomato fest", text: "Little Italia tomato festival", mediaLink: "https://i.pinimg.com/originals/fd/12/d0/fd12d09762450b7cceb5954bf9b6b3c5.jpg"},
     {id: 3, latitude: 41.89580093, longitude: -87.63959151, mediaType: "image", title: "Cuban Cigar", text: "Little Havana Cuban fest", mediaLink: "https://i.amz.mshcdn.com/H4IUTO_uOWyGJ4rwztY2deJU2LM=/fit-in/1200x9600/http%3A%2F%2Fmashable.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fbbblow.gif"},
-    {id: 4, latitude: 41.89370126, longitude: -87.63737755, mediaType: "image", title: "Bike Race", text: "Bicycle racing", mediaLink: "https://i.imgur.com/rskdPAy.gifv"},
+    {id: 4, latitude: 41.89370126, longitude: -87.63737755, mediaType: "image", title: "Bike Race", text: "Bicycle racing", mediaLink: "https://i.imgur.com/rskdPAy.gif"},
     {id: 5, latitude: 41.89671271, longitude: -87.63702022, mediaType: "video", title: "Chicago Marathon", text: "45th Chicago marthon", mediaLink: "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"},
     {id: 6, latitude: 41.89562154, longitude: -87.63920034, mediaType: "image", title: "Sox Suck", text: "The Sox and the Cardinals suck!", mediaLink: "https://i.pinimg.com/originals/db/50/4f/db504fac26d619d859ab5fa8b29d5453.jpg"},
 ]
