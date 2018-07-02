901dc19a68048d4b70f5ffa5bb9b71d6266a66bf
Shaheed Shamsi <shamsishaheed@gmail.com>
2018-07-02 12:09:25 -0500

    backend working

--- server/api/comment.js
+++ server/api/comment.js
@@ -3,10 +3,7 @@ const {Post, User, Comments} = require('../db/models')
 
 router.post('/', async (req, res, next) =>{
     try{
-        console.log('req.body', req.body)
         const post = await Post.findById(req.body.postId)
-        console.log('req.body.postid', req.body.postId)
-        console.log('req.body.userId: ', req.body.userId)
         const newComment = await Comments.create({
             content: req.body.comment,
             postId: req.body.postId,
